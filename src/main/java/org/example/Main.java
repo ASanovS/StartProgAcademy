bd25c572d432312d34c1c3f77b3495336c933a4f
ASanovS <serge.asanov@gmail.com>
2024-02-16 22:25:59 +0200

    Update calculate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        var scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the speed of the car");
+            var speed = scanner.nextDouble();
+
+            System.out.println("Enter the time of the hours on the road");
+            var time = scanner.nextDouble();
+
+            if (speed <= 0 || time <= 0) {
+                System.out.println("Speed or time cannot be less than or equal to zero.");
+                break;
+            }
+
+            var result = speed * time;
+            System.out.println("The car will drive " + result + " kilometers");
         }
     }
 }
